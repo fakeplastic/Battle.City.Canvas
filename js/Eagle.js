22a41cbc0372c52fcecbbe8b5f5a414e59db0e39
Cory Honeyman <coryhoneyman@hotmail.com>
2012-06-07 02:52:34 -0700

    Cory's revisions (start)
    Just getting started makeing revisions.

---
+++ js/Eagle.js
@@ -0,0 +1,10 @@
+/**
+ * @author Cory
+ */
+var Eagle = function(x,y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = new Image();
+  this.sprite.src = "img/NES_Battle_Citty_Eagle.gif";
+  this.isThere = true;
+ }
